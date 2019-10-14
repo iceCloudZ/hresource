8336079bc5e19f0d1248fb4624216fbfe9bf9e7d
jieqin95 <cassie.1995@outlook.com>
2019-10-15 00:20:50 +0800

    update

--- src/main/java/com/jiujiuhouse/resource/controller/HouseController.java
+++ src/main/java/com/jiujiuhouse/resource/controller/HouseController.java
@@ -8,11 +8,15 @@ import com.jiujiuhouse.resource.repository.HouseRepository;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.persistence.*;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author zzh
@@ -37,10 +41,23 @@ public class HouseController {
 
     @ApiOperation(value = "首页列表")
     @GetMapping("/indexList")
-    public JsonResult indexList() {
+    public JsonResult indexList(@RequestParam("index") Integer index) {
+        Sort.Direction sort =  Sort.Direction.ASC;
+        //获取PageRequest对象 index:页码 从0开始  size每页容量 sort排序方式 "id"->properties 以谁为准排序
+        int size = 5;
+        Pageable pageable = PageRequest.of(index, size, sort, "id");
         HouseEntity houseEntity = new HouseEntity();
-        houseEntity.setCityId(15);
-        houseEntity.setCommunityId(11);
+        houseEntity.setId(index);
+        //定义匹配规则 匹配"vendorId"这个属性 exact 精准匹配
+        ExampleMatcher exampleMatcher = ExampleMatcher.matching().withMatcher("id",ExampleMatcher.GenericPropertyMatchers.exact());
+        Example<HouseEntity> example = Example.of(houseEntity,exampleMatcher);
+        Page<HouseEntity> page = houseRepository.findAll(example,pageable);
+        //获取总页数
+        page.getTotalPages();
+        //获取总元素个数
+        page.getTotalElements();
+        //获取该分页的列表
+        page.getContent();
         return JsonResult.create().addResult(houseRepository.findAll());
     }
 }
