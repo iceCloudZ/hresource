ba2dc2f107b871e282917703710591365d435d2c
zihao.zhao <zzh58@vip.qq.com>
2019-11-08 16:37:28 +0800

    fix heartbeat

--- src/main/java/com/jiujiuhouse/resource/HeartBeatController.java
+++ src/main/java/com/jiujiuhouse/resource/HeartBeatController.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+
 /**
  * @author zihao.zhao
  */
@@ -11,9 +13,13 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class HeartBeatController {
 
+    @PostConstruct
+    public void init(){
+        log.info("健康检查服务初始化.");
+    }
+
     @GetMapping("/health")
     public String HeartBeat() {
-        log.info("HeartBeating");
         return "health";
     }
 }
